844793fa6eee3e4fd5febbfcba489c3d2b039b79
rojadhanavath <roja.d293@gmail.com>
2018-11-12 00:11:32 -0500

    added headder page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Footer from './Components/footerComponent'; 
-
+import Footer from './Components/footerComponent';
+import Header from './Components/headerComponent';
 class App extends Component {
   render() {
     return (
        <React.Fragment>
+
+       <Header/>
       <Footer/>
-    
+
       <div className="App">
-  
+
       </div>
         </React.Fragment>
     );
